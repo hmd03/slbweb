83fde4a58335cebbc46b7f9684b8e53d0dd3b336
hmd03 <geg828@nate.com>
2025-03-07 00:38:31 +0900

    feat: FileInput 컴포넌트 추가가

---
+++ src/components/ui/inputs/FileInput.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+interface FileInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  msg: string;
+}
+
+const FileInput: React.FC<FileInputProps> = ({ msg, ...inputProps }) => {
+  const [fileName, setFileName] = useState<string | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setFileName(file.name);
+    } else {
+      setFileName(null);
+    }
+  };
+
+  return (
+    <div className="flex items-center">
+      <input
+        type="file"
+        onChange={handleFileChange}
+        className="hidden"
+        {...inputProps}
+      />
+      <input
+        type="button"
+        value="파일 선택"
+        onClick={() => document.getElementById(inputProps.id!)?.click()}
+        className="bg-gray-300 text-black px-4 py-2 rounded cursor-pointer border border-[2px] border-Black font-bold"
+      />
+      <label
+        htmlFor={inputProps.id}
+        className="cursor-pointer text-Black px-4 py-2"
+      >
+        {fileName || msg}
+      </label>
+    </div>
+  );
+};
+
+export default FileInput;
