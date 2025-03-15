b644d0d0245d410a6e14950a393aa4f785eef3a4
hmd03 <geg828@nate.com>
2025-03-16 00:05:37 +0900

    feat: dateUtils formatDateYYYYMMDD 유틸 추가

--- src/components/utils/dateUtils.tsx
+++ src/components/utils/dateUtils.tsx
@@ -3,6 +3,19 @@ export const formatDate = (isoDatetime: string): string => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
     
-    return `${year}-${month}-${day} 00:00:00`;
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
+
+
+export const formatDateYYYYMMDD = (isoDatetime: string): string => {
+    const date = new Date(isoDatetime);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}`;
 };
