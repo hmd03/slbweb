50b41620f8872d7c90539174a4e568f1fd49f478
hmd03 <geg828@nate.com>
2025-03-20 02:32:06 +0900

    feat: 배너 등록 추가

--- src/components/admin/banner/AdminBannerAddModForm.tsx
+++ src/components/admin/banner/AdminBannerAddModForm.tsx
@@ -6,12 +6,22 @@ import axios from 'axios';
 import Button from '../../ui/buttons/Button';
 import InputField from '../../ui/inputs/InputField';
 import FileInput from '../../ui/inputs/FileInput';
+import RadioButtonGroup from '../../ui/radio/RadioButtonGroup';
+import AlterModal from '../../ui/alters/AlterModal';
+import { useRecoilState } from 'recoil';
+import { LoadingState } from '../../../store/atom';
 
 const AdminBannerAddModForm: React.FC = () => {
     const navigate = useNavigate();
     const { id } = useParams<{ id?: string;}>();
 
     console.log(id);
+
+    const [loading, setLoading] = useRecoilState(LoadingState);
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [isCancelVisible, setIsCancelVisible] = useState(true);
+    const [message, setMessage] = useState('');
+    const [onConfirm, setOnConfirm] = useState(() => () => {});
     
     const titleRef = useRef<HTMLInputElement>(null);
     const linkRef = useRef<HTMLInputElement>(null);
@@ -23,43 +33,60 @@ const AdminBannerAddModForm: React.FC = () => {
     const [imageMsg, setImageMsg] = useState<string>('이미지 사이즈 1920X650');
     const [videoMsg, setVideoMsg] = useState<string>('동영상 사이즈 1920X650');
 
-    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file) {
-            setImageFile(file);
-            const img = new Image();
-            img.src = URL.createObjectURL(file);
-            img.onload = () => {
-                setImageMsg(`${imageMsg}<br>선택한 이미지 사이즈 ${img.width}X${img.height}`);
-            };
-            console.log('선택된 파일:', file);
-        } else {
-            setImageFile(null);
-            setImageMsg('이미지 사이즈 1920X650');
-        }
-    };
+    const [imagePath, setImagePath] = useState<string>('');
 
-    const handleVideoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file) {
-            setVideoFile(file);
-            const video = document.createElement('video');
-            video.src = URL.createObjectURL(file);
-            video.onloadedmetadata = () => {
-                setVideoMsg(`${videoMsg}<br>선택한 동영상 사이즈 ${video.videoWidth}X${video.videoHeight}`);
+    const [selectedOption, setSelectedOption] = useState('0');
+
+    const options = [
+            { id: 'option1', name: 'group1', value: '0', label: 'PC' },
+            { id: 'option2', name: 'group1', value: '1', label: 'MO' },
+        ];
+    
+        const handleChange = (value: React.SetStateAction<string>) => {
+            setSelectedOption(value);
+        };
+
+        const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            const file = event.target.files?.[0];
+            if (file) {
+                setImageFile(file);
+                setImagePath(URL.createObjectURL(file));
+                const img = new Image();
+                img.src = URL.createObjectURL(file);
+                img.onload = () => {
+                    setImageMsg(`${imageMsg}<br>선택한 이미지 사이즈 ${img.width}X${img.height}`);
+                };
+                console.log('선택된 파일:', file);
+            } else {
+                setImageFile(null);
+                setImagePath('');
+                setImageMsg('이미지 사이즈 1920X650');
+            }
+        };
+            
+            
+            const handleVideoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+                const file = event.target.files?.[0];
+                if (file) {
+                    setVideoFile(file);
+                    const video = document.createElement('video');
+                    video.src = URL.createObjectURL(file);
+                    video.onloadedmetadata = () => {
+                        setVideoMsg(`${videoMsg}<br>선택한 동영상 사이즈 ${video.videoWidth}X${video.videoHeight}`);
+                    };
+                    console.log('선택된 파일:', file);
+                } else {
+                    setVideoFile(null);
+                    setVideoMsg('동영상 사이즈 1920X650');
+                }
             };
-            console.log('선택된 파일:', file);
-        } else {
-            setVideoFile(null);
-            setVideoMsg('동영상 사이즈 1920X650');
-        }
-    };
+        
 
     useEffect(() => {
         const fetchData = async () => {
             if (id) {
                 try {
-                    const response = await axios.get(`/api/users/${id}`);
+                    const response = await axios.get(`/api/banners/${id}`);
                     console.log(response);
                     if (response.status === 200) {
                         const data = response.data;
@@ -78,8 +105,127 @@ const AdminBannerAddModForm: React.FC = () => {
         fetchData();
     }, [id]);
 
+    const onSubmit = async () => {
+        const title = titleRef.current?.value || '';
+        const link = linkRef.current?.value || '';
+        const priority = priorityRef.current?.value || '';
+        const duration = durationRef.current?.value || '';
+    
+        const isImageSelected = imageFile != null;
+        const isVideoSelected = videoFile != null;
+    
+        if (
+            title === '' || 
+            (isImageSelected && isVideoSelected) ||
+            (!isImageSelected && !isVideoSelected) ||
+            link === '' || 
+            priority === '' || 
+            duration === ''
+        ) {
+            handleOpenModal(`이미지와 영상 중 하나만 등록 가능, 제목, 우선순위, 재생시간을 다시 확인해주세요.`, false, handleCancel);
+        } else {
+            handleOpenModal(`등록 하시겠습니까?`, true, handleConfirm);
+        }
+    };
+    
+
+    const handleConfirm = async () => {
+        const title = titleRef.current?.value || '';
+        const link = linkRef.current?.value || '';
+        const priority = priorityRef.current?.value || '';
+        const duration = durationRef.current?.value || '';
+        const isMobile = selectedOption === '1' ? 'true' : 'false';
+        const media = imageFile != null ? imageFile : videoFile;
+
+        try {
+            const formData = new FormData();
+                formData.append('title', title);
+                formData.append('isMobile', isMobile);
+                formData.append('link', link);
+                formData.append('priority', priority);
+                formData.append('duration', duration);
+
+                if (media) {
+                    formData.append('media', media);
+                }
+
+                for (const [key, value] of formData.entries()) {
+                    console.log(`${key}: ${value}`);
+                }
+
+            if(!id){
+                setLoading(true);
+                const response = await axios.post(`/api/banners`, formData);
+
+                const data = response.data;
+                setLoading(false);
+    
+                if (response.status === 201) {
+                    navigate('/admin/banner');
+                } else {
+                    alert(data.message);
+                }
+            } else {
+                setLoading(true);
+                const response = await axios.put(`api/banners/${id}`, formData);
+
+                const data = response.data;
+                setLoading(false);
+
+                if (response.status === 200) {
+                    console.log(response);
+                    navigate('/admin/banner');
+                } else {
+                    alert(data.message);
+                }
+            }
+            
+        } catch (error) {
+            alert((error as Error).message);
+        } finally {
+            setLoading(false);
+            setModalVisible(false);
+        }
+    }
+
+    const handleDelClick = () => {
+        handleOpenModal('삭제 하시겠습니까?', true, deleteId);
+    };
+
+    const deleteId = async () => {
+        // try {
+        //     if(id){
+        //         const response = await axios.delete(
+        //             `api/event/${id}`,
+        //           );
+      
+        //           console.log(response)
+        //           const data = response.data;
+      
+        //           if (response.status === 200) {
+        //               navigate('/admin/board/event');
+        //           } else {
+        //               alert(data.message);
+        //           }
+        //     }
+        //   } catch (error) {
+        //     console.log("error: " + error);
+        //   }
+    };
+
+    const handleOpenModal = (msg: string, isCancel = true, confirmFunction: () => void) => {
+        setMessage(msg);
+        setIsCancelVisible(isCancel)
+        setOnConfirm(() => confirmFunction);
+        setModalVisible(true);
+    };
+
+    const handleCancel = () => {
+        setModalVisible(false);
+    }
+
     const onBackPage = () => {
-        navigate('/admin/inquiry');
+        navigate('/admin/banner');
     }
 
     const thClassName = 'bg-LightGray border border-Black border-[2px] p-2 text-left text-center';
@@ -103,6 +249,17 @@ const AdminBannerAddModForm: React.FC = () => {
                             </td>
                         </tr>
                         <tr >
+                            <th className={thClassName}>구분</th>
+                            <td className={tdClassName}>
+                                <RadioButtonGroup
+                                    options={options}
+                                    selectedOption={selectedOption}
+                                    onChange={handleChange}
+                                    className="radio-group flex gap-2"
+                                />
+                            </td>
+                        </tr>
+                        <tr >
                             <th className={thClassName}>배너 링크</th>
                             <td className={`${tdClassName} text-center`}>
                                 <InputField
@@ -116,6 +273,7 @@ const AdminBannerAddModForm: React.FC = () => {
                             <th className={thClassName}>배너 우선순위</th>
                             <td className={`${tdClassName} text-center`}>
                                 <InputField
+                                    type='number'
                                     className=' p-1 w-full border-[2px] '
                                     placeholder='숫자가 작을수록 노출 순서가 빠릅니다.'
                                     ref={priorityRef}
@@ -132,7 +290,7 @@ const AdminBannerAddModForm: React.FC = () => {
                                         accept='image/*'
                                         onChange={handleImageChange}
                                     />
-                                    {imageFile && <img className='ml-10 w-[6.65rem] h-[2.25rem]' id='thumbnail' alt='thumbnail' src={URL.createObjectURL(imageFile)} />}
+                                    {imagePath != '' && <img className='ml-10 w-[6.65rem] h-[2.25rem]' id='thumbnail' alt='thumbnail' src={imagePath} />}
                                 </div>
                             </td>
                         </tr>
@@ -151,6 +309,7 @@ const AdminBannerAddModForm: React.FC = () => {
                             <th className={thClassName}>재생시간(초)</th>
                             <td className={`${tdClassName} text-center`}>
                                 <InputField
+                                    type='number'
                                     className=' p-1 w-full border-[2px] '
                                     placeholder='재생시간을 설정해 주세요.'
                                     ref={durationRef}
@@ -160,11 +319,19 @@ const AdminBannerAddModForm: React.FC = () => {
                     </tbody>
                 </table>
                 <div className='flex w-full items-center h-fit mt-2 gap-2'>
-                    <Button theme='admin' className='px-8 py-[14px] border border-[2px]'>{!id?'등록':'수정'}</Button>
-                    {id && <Button theme='error' className='px-8 py-[14px] border border-[2px]'>삭제</Button>}
+                    <Button onClick={onSubmit} theme='admin' className='px-8 py-[14px] border border-[2px]'>{!id?'등록':'수정'}</Button>
+                    {!!id && <Button onClick={handleDelClick} theme='error' className='px-8 py-[14px] border border-[2px]'>삭제</Button>}
                     <OutlineButton onClick={onBackPage} theme='admin' className='px-8 py-[13px]'>← 목록</OutlineButton>
                 </div>
             </div>
+            {isModalVisible && (
+                <AlterModal
+                    message={message}
+                    isCancelVisible={isCancelVisible}
+                    onConfirm={onConfirm}
+                    onCancel={handleCancel}
+                />
+            )}
         </AdminCurrentLayout>
     );
 };
