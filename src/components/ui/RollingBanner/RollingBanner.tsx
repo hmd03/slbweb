e12677995f5f2a98f84d4e7afee5e4958d2be453
hmd03 <geg828@nate.com>
2025-04-02 23:11:24 +0900

    feat: 롤링 배너 컴포넌트 추가

---
+++ src/components/ui/RollingBanner/RollingBanner.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+
+interface BannerItem {
+  media: string;
+  fileType: string;
+  duration: number; 
+  link: string;
+}
+
+interface RollingBannerProps {
+  items: BannerItem[];
+}
+
+const RollingBanner: React.FC<RollingBannerProps> = ({ items }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [bannerHeight, setBannerHeight] = useState(0);
+
+  useEffect(() => {
+    if (items.length > 0) {
+        setCurrentIndex(0);
+    }
+  }, [items]);
+
+  useEffect(() => {
+    const updateHeight = () => {
+      const width = window.innerWidth;
+      const height = (width * 65) / 192;
+      setBannerHeight(height);
+    };
+
+    updateHeight();
+    window.addEventListener('resize', updateHeight); 
+
+    return () => {
+      window.removeEventListener('resize', updateHeight);
+    };
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
+    }, items[currentIndex]?.duration * 1000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex, items]);
+
+  const handleDotClick = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="relative w-full overflow-hidden" style={{ height: `${bannerHeight}px` }}>
+      {items.map((item, index) => (
+        <div
+          key={index}
+          className={`absolute inset-0 transition-opacity duration-500 ${index === currentIndex ? 'opacity-100' : 'opacity-0'}`}
+        >
+          {item.fileType == 'video' ? (
+            <video
+              style={{ height: `${bannerHeight}px` }}
+              className="w-full object-fill"
+              autoPlay
+              loop
+              muted
+            >
+              <source src={item.media} type="video/mp4" />
+            </video>
+          ) : (
+            <img
+              style={{ height: `${bannerHeight}px` }}
+              className="w-full object-fill"
+              src={item.media}
+              alt={`Banner ${index}`}
+            />
+          )}
+          <a href={item.link} className="absolute inset-0" />
+        </div>
+      ))}
+      <div className="absolute bottom-7 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {items.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => handleDotClick(index)}
+            className={`w-3 h-3 rounded-full border-[1px] border-[#231F20] cursor-pointer 
+            ${ index === currentIndex ? 'bg-Point' : 'bg-white'}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RollingBanner;
