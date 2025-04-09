e959fc00edbac5ec60c235b44341c244355eb911
hmd03 <geg828@nate.com>
2025-04-09 23:15:59 +0900

    feat: 새로운 포인트 섹션 추가 및 디자인 개선

--- src/components/main/main/MainForm.tsx
+++ src/components/main/main/MainForm.tsx
@@ -329,6 +329,258 @@ const MainForm: React.FC = () => {
           </div>
         </div>
       </div>
+      {/* Point 4 */}
+      <div className="flex flex-col items-center w-[1300px] pt-40 text-mainContent font-semibold">
+        <Chip text="Point 4" type="black" />
+        <div className="mt-2 text-subContent  font-black">
+          성공한 맛집 = 인테리어도 좋다!
+        </div>
+        <div className="flex items-end leading-none mb-20 text-mainPoint">
+          고객이 트렌디하다고 말하는
+          <p className="text-[#FF331F] font-black leading-none ml-2">
+            SLB 인테리어
+          </p>
+        </div>
+      </div>
+      <div
+        className="bg-no-repeat bg-center bg-cover flex flex-col items-center w-full text-title font-semibold aspect-[15/10] py-40 px-40 mb-40"
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/main/point_4_background.webp)`,
+        }}
+      >
+        <div className="bg-[#F1F2F2] py-2 px-20 w-fit ml-[25%] break-keep">
+          SLB의 경쟁력 고객이 먼저 알아봅니다.
+        </div>
+        <div className="bg-[#F1F2F2] py-10 px-8 w-[35%] mt-auto mb-40 text-right mr-[30%] break-keep">
+          SLB의 수익형 인테리어
+          <p className="text-[28px] font-normal break-keep">
+            오랜 기간 고객 동선과
+          </p>
+          <p className="text-[28px] font-normal break-keep">
+            서비스 동선까지 고려한 과학적 설계와
+          </p>
+          <p className="text-[28px] font-normal break-keep">
+            우리민족 고유의 절제와 비움에 미학을 반영한
+          </p>
+          <p className="text-[28px] font-normal break-keep">
+            모던하고 감각적인 인테리어 디자인으로
+          </p>
+          <p className="text-[28px] font-normal break-keep">
+            SLB만의 수익형 인테리어를 완성하였습니다.
+          </p>
+        </div>
+      </div>
+      <DividerWithLabel label="Q. 상권선점을 위해서는?" />
+      <div className="flex flex-col items-center leading-none text-title mt-8 mb-40 font-black">
+        <div className="mt-2 text-subContent mb-2">
+          좋은 상권은 기다려주지 않습니다. 놓치지 마세요!
+        </div>
+        <p
+          className="px-4 pt-2 pb-5 text-center text-[#FF331F] font-black text-mainPoint leading-none bg-no-repeat bg-bottom"
+          style={{
+            backgroundImage: `url(${process.env.PUBLIC_URL}/main/point_4_line.webp)`,
+            backgroundSize: "contain",
+            textShadow: "0 0 1px white, 0 0 1px white, 0 0 1px white",
+          }}
+        >
+          SLB를 선점할 수 있는 기회
+        </p>
+      </div>
+      {/* Point 5 6 */}
+      <div className="flex flex-col items-center w-full text-mainContent font-semibold">
+        <div className="flex flex-col items-center w-full text-mainContent font-semibold bg-[#F6F6F6]">
+          {/* Point 5*/}
+          <div className="flex flex-col items-center w-[1300px] pt-20 text-mainContent font-semibold">
+            <Chip text="Point 5" type="black" />
+            <div className="mt-2">각 분야 10년 이상의 전문가가</div>
+            <div className="flex items-end leading-none mb-10 text-mainPoint">
+              <p className="text-[#FF331F] font-black leading-none">
+                창업부터 운영까지 지원
+              </p>
+              하는 SLB
+            </div>
+            <div className="text-[24px] font-normal flex">
+              매장을 직접 운영 했던 경험 보유자부터 홀, 배달 등
+            </div>
+            <div className="text-[24px] font-normal flex">
+              다양한 외식아이템을 경험한
+              <p className="font-bold ml-2">10년 이상의</p>
+            </div>
+            <div className="text-[24px] font-normal flex mb-[8rem]">
+              <p className="font-bold mr-2">
+                각 분야 전문가들이 예비 가맹점주님의 고충을 해결
+              </p>
+              해드립니다.
+            </div>
+            <div className="flex items-center gap-8">
+              <div className="">
+                <img
+                  loading="lazy"
+                  className="w-full rounded-t transition-opacity duration-500 opacity-0"
+                  style={{ boxShadow: "4px 4px 12px rgba(0, 0, 0, 0.2)" }}
+                  onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+                  alt="메뉴 및 운영 노하우 교육육"
+                  src={`${process.env.PUBLIC_URL}/main/point_5_1.webp`}
+                />
+              </div>
+              <div className="">
+                <img
+                  loading="lazy"
+                  className="w-full rounded-t transition-opacity duration-500 opacity-0"
+                  style={{ boxShadow: "4px 4px 12px rgba(0, 0, 0, 0.2)" }}
+                  onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+                  alt="오픈 운영 지원"
+                  src={`${process.env.PUBLIC_URL}/main/point_5_2.webp`}
+                />
+              </div>
+            </div>
+
+            <div className="mb-[16rem]" />
+          </div>
+
+          {/* Point 6 */}
+          <div className="flex flex-col items-center w-[1300px] pt-20 text-mainContent font-semibold">
+            <Chip text="Point 6" type="black" />
+            <div className="mt-2">SLB만의 슈퍼바이징 시스템으로</div>
+            <div className="flex items-end leading-none mb-20 text-mainPoint">
+              오픈 이후에도
+              <p className="text-[#FF331F] font-black leading-none ml-2">
+                체계적으로 관리
+              </p>
+              합니다
+            </div>
+            <div className="flex items-center gap-8 mb-16">
+              <div className="">
+                <img
+                  loading="lazy"
+                  className="w-full rounded-t transition-opacity duration-500 opacity-0"
+                  style={{ boxShadow: "4px 4px 12px rgba(0, 0, 0, 0.2)" }}
+                  onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+                  alt="가맹점 QSC 관리리"
+                  src={`${process.env.PUBLIC_URL}/main/point_6_1.webp`}
+                />
+              </div>
+              <div className="">
+                <img
+                  loading="lazy"
+                  className="w-full rounded-t transition-opacity duration-500 opacity-0"
+                  style={{ boxShadow: "4px 4px 12px rgba(0, 0, 0, 0.2)" }}
+                  onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+                  alt="매출&수익 관리리"
+                  src={`${process.env.PUBLIC_URL}/main/point_6_2.webp`}
+                />
+              </div>
+            </div>
+            <div className="flex items-center gap-8">
+              <div className="">
+                <img
+                  loading="lazy"
+                  className="w-full rounded-t transition-opacity duration-500 opacity-0"
+                  style={{ boxShadow: "4px 4px 12px rgba(0, 0, 0, 0.2)" }}
+                  onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+                  alt="매출향상 LSM 지원원"
+                  src={`${process.env.PUBLIC_URL}/main/point_6_3.webp`}
+                />
+              </div>
+              <div className="">
+                <img
+                  loading="lazy"
+                  className="w-full rounded-t transition-opacity duration-500 opacity-0"
+                  style={{ boxShadow: "4px 4px 12px rgba(0, 0, 0, 0.2)" }}
+                  onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+                  alt="물품입출고 관리리"
+                  src={`${process.env.PUBLIC_URL}/main/point_6_4.webp`}
+                />
+              </div>
+              <div className="">
+                <img
+                  loading="lazy"
+                  className="w-full rounded-t transition-opacity duration-500 opacity-0"
+                  style={{ boxShadow: "4px 4px 12px rgba(0, 0, 0, 0.2)" }}
+                  onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+                  alt="매장인원 관리 지원"
+                  src={`${process.env.PUBLIC_URL}/main/point_6_5.webp`}
+                />
+              </div>
+            </div>
+            <div className="mb-[16rem]" />
+          </div>
+        </div>
+      </div>
+      {/* Point 7 */}
+      <div className="flex flex-col items-center w-[1300px] pt-40 text-mainContent font-semibold">
+        <Chip text="Point 1" type="black" />
+        <div className="mt-2">창업 및 운영 비용은 DOWN</div>
+        <div className="flex items-end leading-none mb-20">
+          <p className="text-[#FF331F] font-black text-mainPoint leading-none">
+            수익률은 UP 시키는 SLB의 노력!
+          </p>
+        </div>
+        <div className="text-[24px] font-normal flex">
+          예비 창업자들의 최대 고민? 창업 비용!
+        </div>
+        <div className="text-[24px] font-normal flex mb-20">
+          SLB는 초기 창업 비용을 줄이기 위해
+          <p className="font-bold ml-2">'인테리어 직거래 시스템' 도입</p>
+        </div>
+        <img
+          loading="lazy"
+          className="mb-80 transition-opacity duration-500 opacity-0"
+          onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+          alt="초기 인테리어 비용 최대 35% 이상 감소"
+          src={`${process.env.PUBLIC_URL}/main/point_7.webp`}
+        ></img>
+        {/* Point 8 */}
+        <Chip text="Point 8" type="black" />
+        <div className="mt-2">물가상승으로 힘들어 하는</div>
+        <div className="">가맹점주님들을 위해 매년 물류 비용</div>
+        <div className="flex items-end leading-none mb-20 text-mainPoint">
+          <p className="text-[#FF331F] font-black leading-none">
+            유지 또는 낮추기 위해 SLB는 노력
+          </p>
+          합니다!
+        </div>
+        <div className="text-[24px] font-normal flex">
+          채소의 안정된 공급과 수익률을 높이기 위해
+        </div>
+        <div className="text-[24px] font-normal flex mb-20">
+          지속적인
+          <p className="font-bold ml-2">무농약 인증 신규 농가 발굴</p>
+        </div>
+        <img
+          loading="lazy"
+          className="mb-80 transition-opacity duration-500 opacity-0"
+          onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+          alt="주6회 배송은 기본! 친환경 무농약 인증 받은 농장과 계약 재배로 사시사철 균일한 가격으로 제공해드립니다."
+          src={`${process.env.PUBLIC_URL}/main/point_8.webp`}
+        ></img>
+        {/* Point 9 */}
+        <Chip text="Point 9" type="black" />
+        <div className="mt-2">누구나 쉽게 쉽게!</div>
+        <div className="flex items-end leading-none mb-20 text-mainPoint">
+          <p className="text-[#FF331F] font-black leading-none mr-2">
+            요리를 전혀 못해도
+          </p>
+          창업 가능
+        </div>
+        <div className="text-[24px] font-normal flex">
+          예비 창업자들의 최대 고민? 창업 비용!
+        </div>
+        <div className="text-[24px] font-normal flex mb-20">
+          SLB는 초기 창업 비용을 줄이기 위해
+          <p className="font-bold ml-2">'인테리어 직거래 시스템' 도입</p>
+        </div>
+        <img
+          loading="lazy"
+          className="mb-80 transition-opacity duration-500 opacity-0"
+          onLoad={(e) => e.currentTarget.classList.add("opacity-100")}
+          alt="주문 시스템 운영 인력 최소화를 위해 키오스크로 편하게! 
+          > 원팩 시스템 - 주로 사용되는 원부자재 원팩화로 준비시간 짧게! 
+          > 간단 조리 시스템 - 어려운 조리 스킬 필요 없이 모든 메뉴 쉽게 가능!
+          > 빠른 메뉴 완성 - 고객 테이블 or 배달 전달 끝!"
+          src={`${process.env.PUBLIC_URL}/main/point_9.webp`}
+        ></img>
+      </div>
     </div>
   );
 };
