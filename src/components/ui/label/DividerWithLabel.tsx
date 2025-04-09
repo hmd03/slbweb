7cf328ebae0efacb54c01a24ccdbcc7913f89dea
hmd03 <geg828@nate.com>
2025-04-10 00:35:11 +0900

    feat: DividerWithLabel 컴포넌트의 반응형 스타일 및 텍스트 색상 개선

--- src/components/ui/label/DividerWithLabel.tsx
+++ src/components/ui/label/DividerWithLabel.tsx
@@ -1,19 +1,29 @@
-// components/DividerWithLabel.tsx
-
 import React from "react";
+import useDeviceInfo from "../../../hooks/useDeviceInfo";
 
 interface DividerWithLabelProps {
   label: string;
 }
 
 const DividerWithLabel: React.FC<DividerWithLabelProps> = ({ label }) => {
+  const deviceInfo = useDeviceInfo();
   return (
-    <div className="flex items-center w-full text-main font-normal">
-      <div className="h-[2px] bg-[#58595B] flex-grow" />
-      <span className="bg-[#58595B] text-white text-title px-4 py-[4px] rounded-tr-[1rem] rounded-bl-[1rem] ml-px mr-px whitespace-nowrap">
+    <div
+      className={`flex items-center w-full text-main font-normal ${
+        deviceInfo.isMobile || deviceInfo.isSmallScreen ? "px-8" : ""
+      }`}
+    >
+      <div className="h-px bg-[#58595B] flex-grow border border-[#58595B]" />
+      <span
+        className={`${
+          deviceInfo.isMobile || deviceInfo.isSmallScreen
+            ? "text-detail"
+            : "text-title"
+        } bg-[#58595B] text-white  px-4 py-[4px] rounded-tr-[1rem] rounded-bl-[1rem] ml-px mr-px whitespace-nowrap`}
+      >
         {label}
       </span>
-      <div className="h-px bg-gray-400 flex-grow" />
+      <div className="h-px bg-[#58595B] flex-grow border border-[#58595B]" />
     </div>
   );
 };
