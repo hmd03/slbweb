ca48e86b0b377fadeac84e659a0f4e22dcfbc678
hmd03 <geg828@nate.com>
2025-04-11 22:50:10 +0900

    feat: setupProxy.js에서 주석 처리된 원격 서버 주소 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
     '/api',
     createProxyMiddleware({
       target: 'http://localhost:3001',
+      //target: 'https://slbmaster3.cafe24.com',
       changeOrigin: true,
     })
   );
