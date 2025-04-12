cbcc4097979b1a9c51643e8a0f7ad834afe45550
hmd03 <hdm030303@gmail.com>
2025-04-12 22:45:25 +0900

    style: useDeviceInfo.tsx에서 코드 포맷 정리 및 작은 화면 기준 수정

--- src/hooks/useDeviceInfo.tsx
+++ src/hooks/useDeviceInfo.tsx
@@ -1,41 +1,45 @@
-import { useEffect, useState } from "react";
-import debounce from "lodash/debounce";
+import { useEffect, useState } from 'react';
+import debounce from 'lodash/debounce';
 
 interface DeviceInfo {
-    isMobile?: boolean;
-    isTouchDevice?: boolean;
-    isSmallScreen?: boolean;
+  isMobile?: boolean;
+  isTouchDevice?: boolean;
+  isSmallScreen?: boolean;
 }
 
 const useDeviceInfo = (): DeviceInfo => {
-    const [deviceInfo, setDeviceInfo] = useState<DeviceInfo>({});
+  const [deviceInfo, setDeviceInfo] = useState<DeviceInfo>({});
 
-    useEffect(() => {
-        const checkDeviceInfo = () => {
-            const userAgent = typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
-            const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-            const isTouchDevice = 'ontouchstart' in window;
-            const isSmallScreen = window.innerWidth <= 950;
+  useEffect(() => {
+    const checkDeviceInfo = () => {
+      const userAgent =
+        typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
+      const isMobile =
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          userAgent
+        );
+      const isTouchDevice = 'ontouchstart' in window;
+      const isSmallScreen = window.innerWidth <= 1024;
 
-            setDeviceInfo({
-                isMobile,
-                isTouchDevice,
-                isSmallScreen,
-            });
-        };
+      setDeviceInfo({
+        isMobile,
+        isTouchDevice,
+        isSmallScreen,
+      });
+    };
 
-        const debouncedCheck = debounce(checkDeviceInfo, 200);
+    const debouncedCheck = debounce(checkDeviceInfo, 200);
 
-        checkDeviceInfo();
-        window.addEventListener('resize', debouncedCheck);
+    checkDeviceInfo();
+    window.addEventListener('resize', debouncedCheck);
 
-        return () => {
-            window.removeEventListener('resize', debouncedCheck);
-            debouncedCheck.cancel();
-        };
-    }, []);
+    return () => {
+      window.removeEventListener('resize', debouncedCheck);
+      debouncedCheck.cancel();
+    };
+  }, []);
 
-    return deviceInfo;
+  return deviceInfo;
 };
 
 export default useDeviceInfo;
