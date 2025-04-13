5f0169ecca6c38bf85f9656818070a2a2359e84b
hmd03 <geg828@nate.com>
2025-04-14 00:25:29 +0900

    style: SeoHelmet 컴포넌트의 메타 태그 정리 및 일관성 있는 형식 적용

--- src/components/common/SeoHelmet.tsx
+++ src/components/common/SeoHelmet.tsx
@@ -8,10 +8,19 @@ const SeoHelmet = () => {
   return (
     <Helmet>
       <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:site_name" content={name} />
+      <meta name='description' content={description} />
+
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:site_name' content={name} />
+      {/* <meta property='og:image' content='https://example.com/og-image.png' /> */}
+      <meta property='og:url' content='https://slbmaster3.cafe24.com/' />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
+      {/* <meta name='twitter:image' content='https://example.com/og-image.png' /> */}
     </Helmet>
   );
 };
