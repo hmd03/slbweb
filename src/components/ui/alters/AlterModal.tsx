7887d80981b7d7e8dbead6e509ae3990ef1b865d
hmd03 <geg828@nate.com>
2025-04-16 23:55:10 +0900

    fix: AlterModal에서 메시지 표시를 위한 CSS 클래스 수정

--- src/components/ui/alters/AlterModal.tsx
+++ src/components/ui/alters/AlterModal.tsx
@@ -9,19 +9,19 @@ interface ModalProps {
 
 const AlterModal: React.FC<ModalProps> = ({ message, isCancelVisible = true, onConfirm, onCancel }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[9999]">
-      <div className="bg-white rounded-lg shadow-lg p-6">
-        <p className="mb-4">{message}</p>
-        <div className="flex justify-end">
+    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[9999] px-10'>
+      <div className='bg-white rounded-lg shadow-lg p-6'>
+        <p className='mb-4 break-keep'>{message}</p>
+        <div className='flex justify-end'>
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+            className='bg-blue-500 text-white px-4 py-2 rounded mr-2'
             onClick={onConfirm}
           >
             확인
           </button>
           {isCancelVisible && (
             <button
-              className="border border-Black bg-White-300 text-black px-4 py-2 rounded"
+              className='border border-Black bg-White-300 text-black px-4 py-2 rounded'
               onClick={onCancel}
             >
               취소
