23aebab89a9572696529136da6c98b7a8071ed48
dmhwang <dmhwang@digentsoft.com>
2025-04-17 11:12:35 +0900

    feat: robots.txt에 사이트맵 추가 및 SeoHelmet에 키워드 메타 태그 추가

--- src/components/common/SeoHelmet.tsx
+++ src/components/common/SeoHelmet.tsx
@@ -5,17 +5,20 @@ import { siteSettingState } from "../../store/atom";
 const SeoHelmet = () => {
   const { name, title, description } = useRecoilValue(siteSettingState);
 
+  const keywords = "슬램북, 창업, 매장관리, 마케팅, 가맹점관리, SLB, 샐러드, 포케";
+
   return (
     <Helmet>
       <title>{title}</title>
       <meta name='description' content={description} />
+      <meta name='keywords' content={keywords} />
 
       <meta property='og:type' content='website' />
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
       <meta property='og:site_name' content={name} />
       <meta property='og:image' content='%PUBLIC_URL%/adminLoginLogo.png' />
-      <meta property='og:url' content='https://slbmaster3.cafe24.com/' />
+      <meta property='og:url' content='https://slbslb.com/' />
 
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:title' content={title} />
