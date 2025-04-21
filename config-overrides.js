cf78fe94ad882a8c977a7299ad57f3301ba9b5e5
dmhwang <dmhwang@digentsoft.com>
2025-04-21 14:40:38 +0900

    fix: output 파일명 및 스타일시트 파일명에 contenthash 사용으로 변경

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,16 @@
 const { override } = require("customize-cra");
 
-module.exports = override((config) => {
-  config.output.filename = 'static/js/[name].[hash:8].js';
-  config.output.chunkFilename = 'static/js/[name].[hash:8].chunk.js';
+module.exports = override(config => {
+  config.output.filename = 'static/js/[name].[contenthash:8].js';
+  config.output.chunkFilename = 'static/js/[name].[contenthash:8].chunk.js';
+
+  const MiniCssExtractPlugin = config.plugins.find(
+    p => p.constructor.name === 'MiniCssExtractPlugin'
+  );
+  if (MiniCssExtractPlugin) {
+    MiniCssExtractPlugin.options.filename = 'static/css/[name].[contenthash:8].css';
+    MiniCssExtractPlugin.options.chunkFilename = 'static/css/[name].[contenthash:8].chunk.css';
+  }
+
   return config;
 });
