c918bc9dc4b1a5f895b9276d8b2f70baf77406ce
SeunghanJI <jsh3581482@naver.com>
2025-04-22 22:01:17 +0900

    Feat : 어드민 리뷰 카드 관리 추가

---
+++ src/pages/admin/reviewCard/AdminReviewCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminReviewCardForm from '../../../components/admin/reviewCard/AdminReviewCardForm';
+
+const AdminReviewCard = () => {
+    return (
+        <div className='w-full h-full flex items-center justify-center'>
+            <AdminReviewCardForm />
+        </div>
+    );
+};
+
+export default AdminReviewCard;
---
+++ src/pages/admin/reviewCard/AdminReviewCardAddMod.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminReviewCardAddModForm from '../../../components/admin/reviewCard/AdminReviewCardAddModForm';
+
+const AdminReviewCardAddMod = () => {
+    return (
+        <div className='w-full h-full flex items-center justify-center'>
+            <AdminReviewCardAddModForm />
+        </div>
+    );
+};
+
+export default AdminReviewCardAddMod;
