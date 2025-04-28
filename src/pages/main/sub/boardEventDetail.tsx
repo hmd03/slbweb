9e1cb634b843087631aec7b2d34d47569720a8c0
hmd03 <geg828@nate.com>
2025-04-29 00:54:50 +0900

    feat: boardEventDetail 컴포넌트 추가

---
+++ src/pages/main/sub/boardEventDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SubBoardEventDetailFrom from '../../../components/main/sub/event/SubBoardEventDetailFrom';
+
+const boardEventDetail = () => {
+  return (
+    <>
+      <SubBoardEventDetailFrom />
+    </>
+  );
+};
+
+export default boardEventDetail;
