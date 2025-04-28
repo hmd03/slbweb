092dfb6e87c720b50885ef688353c4cbbb8e576f
hmd03 <geg828@nate.com>
2025-04-29 00:54:53 +0900

    feat: boardNoticeDetail 컴포넌트 추가

---
+++ src/pages/main/sub/boardNoticeDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SubBoardNoticeDetailFrom from '../../../components/main/sub/notice/SubBoardNoticeDetailFrom';
+
+const boardNoticeDetail = () => {
+  return (
+    <>
+      <SubBoardNoticeDetailFrom />
+    </>
+  );
+};
+
+export default boardNoticeDetail;
