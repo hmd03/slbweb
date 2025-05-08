e0df13caa20d528ee4d70f620c5e43569a08c1ce
dmhwang <dmhwang@digentsoft.com>
2025-05-08 15:57:53 +0900

    fix: 아이디 및 비밀번호 유효성 검사 코드 주석 처리

--- src/components/admin/master/AdminMasterWriteForm.tsx
+++ src/components/admin/master/AdminMasterWriteForm.tsx
@@ -60,24 +60,24 @@ const AdminMasterWriteForm: React.FC = () => {
       setLoading(false);
       return;
     }
-    if (!/^[a-zA-Z]{6,}$/.test(inputid)) {
-      handleOpenModal(
-        '아이디는 영어로 최소 6자 이상이어야 합니다.',
-        false,
-        handleCancel
-      );
-      setLoading(false);
-      return;
-    }
-    if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{6,}$/.test(password)) {
-      handleOpenModal(
-        '비밀번호는 영어, 숫자, 특수문자를 포함하여 최소 6자 이상이어야 합니다.',
-        false,
-        handleCancel
-      );
-      setLoading(false);
-      return;
-    }
+    // if (!/^[a-zA-Z]{6,}$/.test(inputid)) {
+    //   handleOpenModal(
+    //     '아이디는 영어로 최소 6자 이상이어야 합니다.',
+    //     false,
+    //     handleCancel
+    //   );
+    //   setLoading(false);
+    //   return;
+    // }
+    // if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{6,}$/.test(password)) {
+    //   handleOpenModal(
+    //     '비밀번호는 영어, 숫자, 특수문자를 포함하여 최소 6자 이상이어야 합니다.',
+    //     false,
+    //     handleCancel
+    //   );
+    //   setLoading(false);
+    //   return;
+    // }
     if (password !== checkPassword) {
       handleOpenModal(
         '비밀번호와 비밀번호 확인이 일치하지 않습니다.',
