7cf3dd196da646712f820d5e03d93c10a72a86a7
SeunghanJI <jsh3581482@naver.com>
2025-05-09 23:37:43 +0900

    Feat : /upload proxy설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,20 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:3001',
-      //target: 'https://slbmaster3.cafe24.com',
-      changeOrigin: true,
-    })
-  );
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:3001',
+            //target: 'https://slbmaster3.cafe24.com',
+            changeOrigin: true,
+        })
+    );
+    app.use(
+        '/uploads',
+        createProxyMiddleware({
+            target: 'http://localhost:3001',
+            //target: 'https://slbmaster3.cafe24.com',
+            changeOrigin: true,
+        })
+    );
 };
- 
