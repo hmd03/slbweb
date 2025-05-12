22422b785edbc7e7b4c0857bf2c25e13a7d63aae
hmd03 <geg828@nate.com>
2025-05-12 21:41:16 +0900

    feat: FileInput 컴포넌트에 type prop 추가 및 스타일 개선

--- src/components/ui/inputs/FileInput.tsx
+++ src/components/ui/inputs/FileInput.tsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 
 interface FileInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   msg: string;
+  type?: string;
 }
 
-const FileInput: React.FC<FileInputProps> = ({ msg, ...inputProps }) => {
+const FileInput: React.FC<FileInputProps> = ({ msg, type = 'admin', ...inputProps }) => {
   const [fileName, setFileName] = useState<string | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,22 +18,26 @@ const FileInput: React.FC<FileInputProps> = ({ msg, ...inputProps }) => {
   };
 
   return (
-    <div className="flex items-center">
+    <div className='flex items-center'>
       <input
-        type="file"
+        type='file'
         onChange={handleFileChange}
-        className="hidden"
+        className='hidden'
         {...inputProps}
       />
       <input
-        type="button"
-        value="파일 선택"
+        type='button'
+        value='파일 선택'
         onClick={() => document.getElementById(inputProps.id!)?.click()}
-        className="bg-gray-300 text-black px-4 py-2 rounded cursor-pointer border border-[2px] border-Black font-bold"
+        className={`${
+          type === 'main'
+            ? 'bg-White border-[2px] border-[#d1d5db4D]'
+            : 'bg-gray-300 border-[2px] rounded font-bold border-Black'
+        }  text-black px-4 py-2 cursor-pointer border `}
       />
       <label
         htmlFor={inputProps.id}
-        className="cursor-pointer text-Black px-4 py-2 whitespace-pre-line text-left"
+        className='cursor-pointer text-Black px-4 py-2 whitespace-pre-line text-left'
       >
         {fileName || msg}
       </label>
